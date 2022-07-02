2f1a6e2e9cb75559eaabeadeac1129c257aee692
Suresh Kumar Gadi <skgadi@users.noreply.github.com>
2022-07-01 20:03:26 -0500

    Automatic push

---
+++ dev/script/004-snackbar.js
@@ -0,0 +1,6 @@
+function showSnackbarMessage(msg) {
+  var x = document.getElementById("snackbar");
+  x.innerHTML = msg;
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
--- dev/script/010-vue.js
+++ dev/script/010-vue.js
@@ -37,6 +37,14 @@ const app = createApp({
         this.saveSettings();
       },
       deep: true
+    },
+    's.rpf' (newVal) {
+      this.s.rpf = getValidInput(newVal, 0.001, 1);
+      this.adjustRpf();
+      this.mainCalculations();
+      this.saveSettings();
+    }, 's.rpfs' (newVal) {
+      this.adjustRpf();
     }
   },
   methods: {
@@ -51,6 +59,21 @@ const app = createApp({
       this.s.s = this.s.w/this.s.cpf;
       this.s.q = Math.sqrt(Math.pow(this.s.s,2) - Math.pow(this.s.w,2));
       this.s.i =  this.s.s / this.s.vol / Math.sqrt(this.s.nP);
+      this.s.rS = this.s.w/this.s.rpf;
+      this.s.rQ = this.s.rS * Math.sqrt(1-Math.pow(this.s.rpf,2));
+      this.s.rI = this.s.rS / this.s.vol / Math.sqrt(this.s.nP);
+      this.s.rQC = this.s.q + this.s.rpfs*this.s.rQ;
+      let xc = Math.pow(this.s.vol,2) / this.s.rQC;
+      this.s.c = 1e6 / (2 * Math.PI * this.s.f * xc);
+    }, adjustRpf() {
+      if (this.s.rpfs<0) {
+        if (this.s.rpf<this.s.cpf) {
+          this.s.rpf = this.s.cpf;
+          showSnackbarMessage(this.l[this.s.sL][24]);
+        }
+      }
+      this.mainCalculations();
+      this.saveSettings();
     }
   }
 }).mount('#gsk-app');
