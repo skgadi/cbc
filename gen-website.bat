d1d5f31dcabf59fd8fa779be4cb36dab8d59e4e9
Suresh Kumar Gadi <skgadi@users.noreply.github.com>
2022-07-02 20:01:34 -0500

    Automatic push

--- gen-website.bat
+++ gen-website.bat
@@ -1,3 +1,14 @@
-git add .
-git commit -am "Automatic push"
-git push
+rmdir /s/q .\docs
+mkdir .\docs\
+copy .\dev\*.png .\docs\
+copy .\dev\*.js .\docs\
+copy .\dev\manifest.json .\docs\
+copy .\dev\CNAME .\docs\
+copy .\dev\browserconfig.xml .\docs\
+copy .\dev\index.min.html .\docs\index.html
+xcopy .\dev\flags .\docs\flags /y/s/q/i
+mkdir .\docs\css\
+copy .\dev\css\*.min.css .\docs\css\
+
+
+
