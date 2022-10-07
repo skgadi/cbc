11ee0d12a14b1e452b72254216ddc69febfb8b38
Suresh Kumar Gadi <skgadi@users.noreply.github.com>
2022-10-07 18:17:29 -0500

    Automatic push

--- dev/script/010-vue.js
+++ dev/script/010-vue.js
@@ -14,21 +14,21 @@ const app = createApp({
       this.saveSettings();
     },
     's.vol' (newVal) {
-      this.s.vol = getValidInput(newVal, 0.001, Infinity);
+      //this.s.vol = getValidInput(newVal, 0.001, Infinity);
       this.mainCalculations();
       this.saveSettings();
     },
     's.f' (newVal) {
-      this.s.f = getValidInput(newVal, 0.001, Infinity);
+      //this.s.f = getValidInput(newVal, 0.001, Infinity);
       this.saveSettings();
     },
     's.w' (newVal) {
-      this.s.w = getValidInput(newVal, 0.001, Infinity);
+      //this.s.w = getValidInput(newVal, 0.001, Infinity);
       this.mainCalculations();
       this.saveSettings();
     },
     's.cpf' (newVal) {
-      this.s.cpf = getValidInput(newVal, 0.001, 1);
+      //this.s.cpf = getValidInput(newVal, 0.001, 1);
       this.mainCalculations();
       this.saveSettings();
     },
@@ -39,7 +39,7 @@ const app = createApp({
       deep: true
     },
     's.rpf' (newVal) {
-      this.s.rpf = getValidInput(newVal, 0.001, 1);
+      //this.s.rpf = getValidInput(newVal, 0.001, 1);
       this.adjustRpf();
       this.mainCalculations();
       this.saveSettings();
@@ -72,6 +72,32 @@ const app = createApp({
       this.s.cD = 1e6 / (2 * Math.PI * this.s.f * xc);
       xc = Math.pow(this.s.vol/Math.sqrt(3),2) / this.s.rQC * 3;
       this.s.cS = 1e6 / (2 * Math.PI * this.s.f * xc);
+
+      //Calculating quantities for the third tab
+      // one phase
+      this.s.ncs = Math.ceil(this.s.vol/this.s.av); //number of capacitors in series
+      this.s.ncp = Math.ceil(this.s.c/(this.s.ac/this.s.ncs)); //number of capacitors in parallel
+      let realCapacitor = (this.s.ncp*this.s.ac/this.s.ncs)*1e-6; //real capacitance added in uF
+      this.s.cri = Math.pow(this.s.vol, 2) * 2 * Math.PI * this.s.f * realCapacitor; // reactive power
+      this.s.fr = this.s.cri - this.s.q;
+      this.s.fa = Math.sqrt(Math.pow(this.s.fr,2) + Math.pow(this.s.w,2));
+      this.s.fpf = this.s.w / this.s.fa;
+      // three phase star
+      this.s.ncss = Math.ceil(this.s.vol/this.s.av/Math.sqrt(3)); //number of capacitors in series
+      this.s.ncps = Math.ceil(this.s.cS/(this.s.ac/this.s.ncss)); //number of capacitors in parallel
+      realCapacitor = (this.s.ncps*this.s.ac/this.s.ncss)*1e-6; //real capacitance added in uF
+      this.s.cris = 3 * Math.pow(this.s.vol/Math.sqrt(3), 2) * 2 * Math.PI * this.s.f * realCapacitor; // reactive power
+      this.s.frs = this.s.cris - this.s.q;
+      this.s.fas = Math.sqrt(Math.pow(this.s.frs,2) + Math.pow(this.s.w,2));
+      this.s.fpfs = this.s.w / this.s.fas;     
+      // three phase delta
+      this.s.ncsd = Math.ceil(this.s.vol/this.s.av); //number of capacitors in series
+      this.s.ncpd = Math.ceil(this.s.cS/(this.s.ac/this.s.ncss)); //number of capacitors in parallel
+      realCapacitor = (this.s.ncpd*this.s.ac/this.s.ncsd)*1e-6; //real capacitance added in uF
+      this.s.crid = 3 * Math.pow(this.s.vol, 2) * 2 * Math.PI * this.s.f * realCapacitor; // reactive power
+      this.s.frd = this.s.crid - this.s.q;
+      this.s.fad = Math.sqrt(Math.pow(this.s.frd,2) + Math.pow(this.s.w,2));
+      this.s.fpfd = this.s.w / this.s.fad;
     }, adjustRpf() {
       if (this.s.rpfs<0) {
         if (this.s.rpf<this.s.cpf) {
